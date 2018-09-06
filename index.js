77894e83ee47acf9713ec6424a713b5bf7321871
yuhaiqing <1243305344@qq.com>
2018-09-06 11:43:23 +0800

    no message

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-
+// 组件
 export { default as BasicLayout } from './lib/layouts/BasicLayout';
-export { default as request } from './lib/utils/request';
-export { default as queryTool } from './lib/common/queryTool';
 export { default as PageHeaderLayout } from './lib/layouts/PageHeaderLayout';
+
+
+
+
+
+
+
+
+
+
+// 方法
+export { default as request } from './lib/utils/request';
+export {filterQueryData} from './lib/common/queryTool';
