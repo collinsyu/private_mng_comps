c4d2b01486562352e0bbcc2e0378d186bf47437d
yuhaiqing <1243305344@qq.com>
2018-09-06 10:32:08 +0800

    asd

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var BasicLayout = require("./lib/layouts/BasicLayout");
 
-
-
-
-exports.BasicLayout = BasicLayout;
+export { default as BasicLayout } from './lib/layouts/BasicLayout';
+export { default as request } from './lib/utils/request';
+export { default as queryTool } from './lib/common/queryTool';
+export { default as PageHeaderLayout } from './lib/layouts/PageHeaderLayout';
