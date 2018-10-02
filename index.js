8b948c5936db3ffa9c93084d714634dcf801a3ff
yuhaiqing <1243305344@qq.com>
2018-10-02 15:11:36 +0800

    20181002

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 // 组件
 
+
+// layouts
 export { default as BasicLayout } from './lib/layouts/BasicLayout';
+export { default as PageHeaderLayout } from './lib/layouts/PageHeaderLayout';
+
+// components
+export { default as SelectX } from './lib/components/SelectX';
+export { default as SlipOutModal } from './lib/components/SlipOutModal';
+export { default as CheckboxX } from './lib/components/CheckboxX';
+export { default as DbclickCopySpan } from './lib/components/DbclickCopySpan';
+export { default as EditSpanCell } from './lib/components/EditSpanCell';
+export { default as RangePickerX } from './lib/components/RangePickerX';
+export { default as RowCallList } from './lib/components/RowCallList';
+export { default as FormItemX } from './lib/components/FormItemX';
+export { default as TableX } from './lib/components/TableX';
+export { default as SearchTable } from './lib/components/SearchTable';
+export { default as QuickTool } from './lib/components/QuickTool';
+export { default as UploadImageX } from './lib/components/UploadImageX';
+export { default as WrapAuth } from './lib/components/WrapAuth';
+export { default as AuthA } from './lib/components/AuthA';
+export { default as EditableCell } from './lib/components/EditableCell';
+export { default as SearchTree } from './lib/components/SearchTree';
 
 
 
@@ -11,6 +32,6 @@ export { default as BasicLayout } from './lib/layouts/BasicLayout';
 
 
 
-// 方法
+// 方法 utils
 export { default as request } from './lib/utils/request';
 export {filterQueryData} from './lib/common/queryTool';
