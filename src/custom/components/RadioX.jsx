e0572a2e87fe4d54860dd19efd8cea5cb25ac3cb
yuhaiqing <1243305344@qq.com>
2019-03-19 16:35:19 +0800

    Initial commit

--- src/custom/components/RadioX.jsx
+++ src/custom/components/RadioX.jsx
@@ -1,9 +1,15 @@
 import React, { PureComponent } from 'react';
 import { Radio } from 'antd';
 const RadioGroup = Radio.Group;
+const RadioButton = Radio.Button;
 
 class RadioX extends PureComponent {
   renderRadio = (options) => {
+    const {style} = this.props;
+    if(style === "button"){
+      return options.map(opt => <RadioButton key={opt.value} value={opt.value}>{opt.name}</RadioButton>);
+
+    }
     return options.map(opt => <Radio key={opt.value} value={opt.value}>{opt.name}</Radio>);
   }
 
