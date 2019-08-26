8769691167c55d44d89900814b180dc55e58e407
校长於伯适 <1243305344@qq.com>
2019-08-26 18:32:10 +0800

    add portal to draw

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,9 +1,44 @@
 import React, { PureComponent } from 'react';
+import ReactDOM from "react-dom";
 import { Icon} from 'antd'
 import QueueAnim from 'rc-queue-anim';
 import Debounce from 'lodash-decorators/debounce';
 
-class Drawer extends PureComponent {
+
+
+
+
+
+class Drawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount() {
+    // The portal element is inserted in the DOM tree after
+    // the Modal's children are mounted, meaning that children
+    // will be mounted on a detached DOM node. If a child
+    // component requires to be attached to the DOM tree
+    // immediately when mounted, for example to measure a
+    // DOM node, or uses 'autoFocus' in a descendant, add
+    // state to Modal and only render the children when Modal
+    // is inserted in the DOM tree.
+    document.body.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      <DrawerInner {...this.props}/>,
+      this.el,
+    );
+  }
+}
+class DrawerInner extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
