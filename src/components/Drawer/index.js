574be1213d5c109c0dfd9e6b2c54849f3b477645
校长於伯适 <1243305344@qq.com>
2019-09-06 17:06:42 +0800

    fix bug

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Icon} from 'antd'
 import QueueAnim from 'rc-queue-anim';
 import Debounce from 'lodash-decorators/debounce';
-
+import style from "./index.less"
 
 
 
@@ -88,26 +88,26 @@ class DrawerInner extends PureComponent {
   render() {
     var width = this.state.width || this.props.width;
     return (
-      <div className={"_yhq_drawer"}>
+      <div className={style._yhq_drawer}>
         <QueueAnim key="drawer" type={['right', 'right']} ease={['easeOutQuart', 'easeInOutQuart']}>
           {this.props.visible
             ? (
-              <div key='absolute' className={"_yhq_absolute"}>
-                <div className={"_yhq_masklayer"} onClick={this.props.onCancel}></div>
-                <div className={"_yhq_container"} style={{width:width}} ref={container=>this.container = container}>
-                  <div className="_yhq_leftborder"
+              <div key='absolute' className={style._yhq_absolute}>
+                <div className={style._yhq_masklayer} onClick={this.props.onCancel}></div>
+                <div className={style._yhq_container} style={{width:width}} ref={container=>this.container = container}>
+                  <div className="style.yhq_leftborder"
                   onMouseDown={this.onMouseDown}
                   onMouseUp={this.onMouseUp}></div>
                   <div style={{flex:"1 0",width:"100%",overflow:"hidden"}}>
-                    <div className={"_yhq_head"}>
-                      <span className={"_yhq_title"}>{this.state.title}</span>
-                      <Icon onClick={this.props.onCancel} className={"_yhq_close"} type="close"/>
+                    <div className={style._yhq_head}>
+                      <span className={style._yhq_title}>{this.state.title}</span>
+                      <Icon onClick={this.props.onCancel} className={style._yhq_close} type="close"/>
 
                     </div>
-                    <div className={"_yhq_body"} style={{maxHeight: document.body.clientHeight-(this.props.footString?80:40)}}>
+                    <div className={style._yhq_body} style={{maxHeight: document.body.clientHeight-(this.props.footString?80:40)}}>
                       {this.props.children}
                     </div>
-                    {this.props.footString?<div className={"_yhq_foot"}>
+                    {this.props.footString?<div className={style._yhq_foot}>
                       {this.props.footString}
                     </div>:null}
                   </div>
@@ -117,91 +117,7 @@ class DrawerInner extends PureComponent {
             )
             : null}
         </QueueAnim>
-        <style>{`
-          ._yhq_drawer{
-            display:inline-block;
-          }
-          ._yhq_drawer ._yhq_absolute {
-            position: fixed;
-            top: 0;
-            right: 0;
-            height: 100%;
-            width: 100%;
-            z-index: 99;
-          }
-          ._yhq_drawer ._yhq_masklayer {
-            z-index: 9999;
-            position: fixed;
-            top: 0;
-            right: 0;
-            height: 100%;
-            width: 100%;
-          }
-          ._yhq_drawer ._yhq_container {
-            display:flex;
-            -moz-box-shadow: -1px 0 20px #757474;
-            -webkit-box-shadow: -1px 0 20px #757474;
-            box-shadow: -1px 0 20px #757474;
-            border-left: 1px solid #dedede;
-            z-index: 100000;
-            position: fixed;
-            top: 0;
-            right: 0;
-            height: 100%;
-            width: 50%;
-            background: #fff;
-          }
-          ._yhq_leftborder{
-            width: 3px;
-            flex-shrink: 0;
-            margin-left: -1.5px;
-          }
-          ._yhq_leftborder:hover{
-            cursor:ew-resize;
-          }
-          ._yhq_drawer ._yhq_body {
-            padding: 20px;
-            overflow-y: auto;
-          }
-          ._yhq_drawer ._yhq_foot {
-            position: absolute;
-            bottom: 0;
-            height: 40px;
-            line-height: 40px;
-            text-align: center;
-            width: 100%;
-            border-top: 1px solid #dedede;
-          }
-          ._yhq_drawer ._yhq_head  {
-            height: 40px;
-            line-height: 40px;
-            border-bottom: 1px solid #dedede;
-          }
-          ._yhq_drawer ._yhq_head ._yhq_title {
-            padding: 0 10px;
-            font-size: large;
-            line-height: 40px;
-          }
-          ._yhq_drawer ._yhq_head ._yhq_close {
-            float: right;
-            line-height: 40px;
-            padding: 0 10px;
-            font-size: 22px;
-            cursor: pointer;
-            transition: All 0.4s ease-in-out;
-            -webkit-transition: All 0.4s ease-in-out;
-            -moz-transition: All 0.4s ease-in-out;
-            -o-transition: All 0.4s ease-in-out;
-          }
-          ._yhq_drawer ._yhq_head ._yhq_close:hover {
-            transform: rotate(180deg);
-            -webkit-transform: rotate(180deg);
-            -moz-transform: rotate(180deg);
-            -o-transform: rotate(180deg);
-            -ms-transform: rotate(180deg);
-          }
-
-        `}</style>
+        
       </div>
     );
   }
