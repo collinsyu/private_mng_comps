6a837256c6d3a6eac58b769aab9569a8ffe7a8bf
校长於伯适 <1243305344@qq.com>
2019-09-10 12:44:07 +0800

    ui

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,49 +1,19 @@
 import React, { PureComponent } from 'react';
 import ReactDOM from "react-dom";
-import { Icon} from 'antd'
+import { Icon ,Drawer as DDrawer} from 'antd'
 import QueueAnim from 'rc-queue-anim';
 import Debounce from 'lodash-decorators/debounce';
-import style from "./index.less"
+// import style from "./index.less"
 
 
 
 
 
-class Drawer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.el = document.createElement('div');
-  }
-
-  componentDidMount() {
-    // The portal element is inserted in the DOM tree after
-    // the Modal's children are mounted, meaning that children
-    // will be mounted on a detached DOM node. If a child
-    // component requires to be attached to the DOM tree
-    // immediately when mounted, for example to measure a
-    // DOM node, or uses 'autoFocus' in a descendant, add
-    // state to Modal and only render the children when Modal
-    // is inserted in the DOM tree.
-    document.body.appendChild(this.el);
-  }
-
-  componentWillUnmount() {
-    document.body.removeChild(this.el);
-  }
-
-  render() {
-    return ReactDOM.createPortal(
-      <DrawerInner {...this.props}/>,
-      this.el,
-    );
-  }
-}
-class DrawerInner extends PureComponent {
+class Drawer extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       visible: this.props.visible,
-      title: this.props.title || '',
       // footString: this.props.footString || '',
       buttonLabel: this.props.buttonLabel || '',
       width:0,
@@ -56,6 +26,7 @@ class DrawerInner extends PureComponent {
 
   componentWillUnmount = ()=>{
     document.removeEventListener("mousemove",this.onMouseMove)
+    document.removeEventListener("mouseup",this.onMouseUp)
   }
   @Debounce(400)
   onMouseMove = (e)=>{
@@ -67,16 +38,19 @@ class DrawerInner extends PureComponent {
       if(width>=document.body.clientWidth){
         width = document.body.clientWidth - 100
       }
-      this.container.style.width = width + "px";
+      this.resize(width + "px")
+      // this.container.style.width = width + "px";
     }
   }
   onMouseDown = ()=>{
     document.addEventListener("mousemove",this.onMouseMove)
     this.ishold = true; 
-    
+    document.addEventListener("mouseup",this.onMouseUp)
   }
   onMouseUp = ()=>{
     document.removeEventListener("mousemove",this.onMouseMove)
+    document.removeEventListener("mouseup",this.onMouseUp)
+
     this.ishold = false;
     console.warn("up");
     
@@ -86,39 +60,39 @@ class DrawerInner extends PureComponent {
     this.setState({width})
   }
   render() {
-    var width = this.state.width || this.props.width;
+    var width = this.state.width || this.props.width || "50%" ;
     return (
-      <div className={style._yhq_drawer}>
-        <QueueAnim key="drawer" type={['right', 'right']} ease={['easeOutQuart', 'easeInOutQuart']}>
-          {this.props.visible
-            ? (
-              <div key='absolute' className={style._yhq_absolute}>
-                <div className={style._yhq_masklayer} onClick={this.props.onCancel}></div>
-                <div className={style._yhq_container} style={{width:width}} ref={container=>this.container = container}>
-                  <div className="style.yhq_leftborder"
-                  onMouseDown={this.onMouseDown}
-                  onMouseUp={this.onMouseUp}></div>
-                  <div style={{flex:"1 0",width:"100%",overflow:"hidden"}}>
-                    <div className={style._yhq_head}>
-                      <span className={style._yhq_title}>{this.state.title}</span>
-                      <Icon onClick={this.props.onCancel} className={style._yhq_close} type="close"/>
 
-                    </div>
-                    <div className={style._yhq_body} style={{maxHeight: document.body.clientHeight-(this.props.footString?80:40)}}>
-                      {this.props.children}
-                    </div>
-                    {this.props.footString?<div className={style._yhq_foot}>
-                      {this.props.footString}
-                    </div>:null}
-                  </div>
-
-                </div>
-              </div>
-            )
-            : null}
-        </QueueAnim>
-        
-      </div>
+      <DDrawer
+        width={width}
+        closable
+        title={this.props.title}
+        placement="right"
+        closable={false}
+        onClose={this.props.onCancel}
+        visible={this.props.visible}
+        // className={style.outcontainer}
+        bodyStyle={{padding:0}}
+      >
+        <div style={{display:"flex"}}>
+          <div className={"_yhq_leftborder"} onMouseDown={this.onMouseDown}></div>
+          <div style={{flex:"1 0",width:"100%",padding:24}}>{this.props.children}</div>
+        </div>
+        <style>{`
+        ._yhq_leftborder{
+          width: 3px;
+          flex-shrink: 0;
+          margin-left: -1.5px;
+          position: absolute;
+          left: 0;
+          height: 100%;
+        }
+        ._yhq_leftborder:hover{
+          cursor:ew-resize;
+        }
+        `}</style>
+      </DDrawer>
+      
     );
   }
 
